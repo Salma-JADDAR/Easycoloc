b2b8992b90e842bc6046a16486b864a7d2c072f9
Salma GTU <salmajaddar7@gmail.com>
2026-02-27 15:20:55 +0100

    ajout de la classe RequestLogin et enregistrement du middleware CheckBanned dans le Kernel pour activer la vérification des utilisateurs bannis

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -9,23 +9,19 @@ use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
 
-class LoginRequest extends FormRequest
-{
-    public function authorize(): bool
-    {
+class LoginRequest extends FormRequest{
+    public function authorize(): bool{
         return true;
     }
 
-    public function rules(): array
-    {
+    public function rules(): array{
         return [
             'email' => ['required', 'string', 'email'],
             'password' => ['required', 'string'],
         ];
     }
 
-    public function authenticate(): void
-    {
+    public function authenticate(): void{
         $this->ensureIsNotRateLimited();
 
         if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
@@ -39,8 +35,7 @@ class LoginRequest extends FormRequest
         RateLimiter::clear($this->throttleKey());
     }
 
-    protected function ensureIsNotRateLimited(): void
-    {
+    protected function ensureIsNotRateLimited(): void{
         if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
             return;
         }
@@ -57,8 +52,7 @@ class LoginRequest extends FormRequest
         ]);
     }
 
-    protected function throttleKey(): string
-    {
+    protected function throttleKey(): string{
         return Str::transliterate(Str::lower($this->input('email')).'|'.$this->ip());
     }
 }
